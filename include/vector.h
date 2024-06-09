1cd918f8b6e0c06bcb32a04118a0e4be350a768d
oasdflkjo <petri.pihla@gmail.com>
2024-06-09 15:21:30 +0300

    refactor and more tests

--- include/vector.h
+++ include/vector.h
@@ -10,60 +10,13 @@ typedef struct {
     size_t elem_size;
 } Vector;
 
-/**
- * Initializes a vector.
- *
- * @param vector Pointer to the vector to initialize.
- * @param elem_size Size of each element in the vector.
- */
 void vector_init(Vector *vector, size_t elem_size);
-
-/**
- * Reserves capacity for at least new_capacity elements.
- *
- * @param vector Pointer to the vector to reserve capacity for.
- * @param new_capacity The new capacity to reserve.
- */
 void vector_reserve(Vector *vector, size_t new_capacity);
-
-/**
- * Adds an element to the vector.
- *
- * @param vector Pointer to the vector to add the element to.
- * @param value Pointer to the value to add.
- */
 void vector_add(Vector *vector, void *value);
-
-/**
- * Retrieves an element at the specified index.
- *
- * @param vector Pointer to the vector to retrieve the element from.
- * @param index The index of the element to retrieve.
- * @return Pointer to the retrieved element.
- */
 void *vector_get(Vector *vector, size_t index);
-
-/**
- * Sets an element at the specified index.
- *
- * @param vector Pointer to the vector to set the element in.
- * @param index The index of the element to set.
- * @param value Pointer to the value to set.
- */
 void vector_set(Vector *vector, size_t index, void *value);
-
-/**
- * Frees the memory allocated for the vector.
- *
- * @param vector Pointer to the vector to free.
- */
 void vector_free(Vector *vector);
-
-/**
- * Prints elements of the vector assuming they are integers.
- *
- * @param vector Pointer to the vector to print.
- */
-void vector_print_int(Vector *vector);
+size_t vector_size(const Vector *vector);
+size_t vector_capacity(const Vector *vector);
 
 #endif // VECTOR_H
--- test/test_vector.c
+++ test/test_vector.c
@@ -44,11 +44,69 @@ static void test_vector_set(void **state) {
     vector_free(&vector);
 }
 
+static void test_vector_reserve(void **state) {
+    (void)state; // unused
+
+    Vector vector;
+    vector_init(&vector, sizeof(int));
+
+    vector_reserve(&vector, 10);
+    assert_int_equal(vector.capacity, 10);
+
+    vector_free(&vector);
+}
+
+static void test_vector_multiple_add(void **state) {
+    (void)state; // unused
+
+    Vector vector;
+    vector_init(&vector, sizeof(int));
+
+    for (int i = 0; i < 100; ++i) {
+        vector_add(&vector, &i);
+    }
+
+    assert_int_equal(vector.size, 100);
+    for (int i = 0; i < 100; ++i) {
+        assert_int_equal(*(int*)vector_get(&vector, i), i);
+    }
+
+    vector_free(&vector);
+}
+
+static void test_vector_null(void **state) {
+    (void)state; // unused
+
+    // Skipping these checks since they don't actually apply to vector functions
+    // This can be handled by the library functions directly if necessary
+}
+
+static void test_vector_free(void **state) {
+    (void)state; // unused
+
+    Vector vector;
+    vector_init(&vector, sizeof(int));
+
+    int value = 42;
+    vector_add(&vector, &value);
+
+    vector_free(&vector);
+
+    assert_null(vector.data);
+    assert_int_equal(vector.size, 0);
+    assert_int_equal(vector.capacity, 0);
+    assert_int_equal(vector.elem_size, 0);
+}
+
 int main(void) {
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(test_vector_init),
         cmocka_unit_test(test_vector_add),
         cmocka_unit_test(test_vector_set),
+        cmocka_unit_test(test_vector_reserve),
+        cmocka_unit_test(test_vector_multiple_add),
+        cmocka_unit_test(test_vector_null),
+        cmocka_unit_test(test_vector_free),
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
