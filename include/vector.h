fd0d9141764286eaab51077fbcb55c63b7b33f72
oasdflkjo <petri.pihla@gmail.com>
2024-06-09 14:58:27 +0300

    fast robust refactor

--- include/vector.h
+++ include/vector.h
@@ -10,11 +10,25 @@ typedef struct {
     size_t elem_size;
 } Vector;
 
+// Initialize the vector with the element size
 void vector_init(Vector *vector, size_t elem_size);
+
+// Reserve memory for a given capacity
+void vector_reserve(Vector *vector, size_t new_capacity);
+
+// Add an element to the vector
 void vector_add(Vector *vector, void *value);
+
+// Get an element at a specific index
 void *vector_get(Vector *vector, size_t index);
+
+// Set an element at a specific index
 void vector_set(Vector *vector, size_t index, void *value);
+
+// Free the memory allocated for the vector
 void vector_free(Vector *vector);
+
+// Print elements of the vector assuming they are integers
 void vector_print_int(Vector *vector);
 
 #endif // VECTOR_H
--- src/vector.c
+++ src/vector.c
@@ -9,25 +9,50 @@ void vector_init(Vector *vector, size_t elem_size) {
     vector->capacity = 4;
     vector->elem_size = elem_size;
     vector->data = malloc(vector->capacity * elem_size);
+    if (vector->data == NULL) {
+        fprintf(stderr, "Failed to allocate memory\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void vector_reserve(Vector *vector, size_t new_capacity) {
+    if (new_capacity > vector->capacity) {
+        void *new_data = realloc(vector->data, new_capacity * vector->elem_size);
+        if (new_data == NULL) {
+            fprintf(stderr, "Failed to reallocate memory\n");
+            exit(EXIT_FAILURE);
+        }
+        vector->data = new_data;
+        vector->capacity = new_capacity;
+    }
 }
 
 void vector_add(Vector *vector, void *value) {
+    if (vector == NULL) {
+        fprintf(stderr, "Vector is NULL\n");
+        exit(EXIT_FAILURE);
+    }
     if (vector->size == vector->capacity) {
-        vector->capacity *= 2;
-        vector->data = realloc(vector->data, vector->capacity * vector->elem_size);
+        vector_reserve(vector, vector->capacity == 0 ? 4 : vector->capacity * 2);
     }
     void *target = (char*)vector->data + (vector->size * vector->elem_size);
     memcpy(target, value, vector->elem_size);
     vector->size++;
 }
 
-void *vector_get(Vector *vector, size_t index) {
-    assert(index < vector->size);
+inline void *vector_get(Vector *vector, size_t index) {
+    if (index >= vector->size) {
+        fprintf(stderr, "Index out of bounds\n");
+        exit(EXIT_FAILURE);
+    }
     return (char*)vector->data + (index * vector->elem_size);
 }
 
-void vector_set(Vector *vector, size_t index, void *value) {
-    assert(index < vector->size);
+inline void vector_set(Vector *vector, size_t index, void *value) {
+    if (index >= vector->size) {
+        fprintf(stderr, "Index out of bounds\n");
+        exit(EXIT_FAILURE);
+    }
     void *target = (char*)vector->data + (index * vector->elem_size);
     memcpy(target, value, vector->elem_size);
 }
